4b4adf2726c488c8f6a2fb96347eb0441d023968
Natã Giertyas da Silva <natadasilva10@gmail.com>
2024-09-20 00:21:06 -0300

    FEAT - Create endpoint to receceive github push

--- src/server.js
+++ src/server.js
@@ -18,6 +18,24 @@ app.post("/send-message-any-discord", async (req, res) => {
   res.status(200).send("Mensagem processada.");
 });
 
+app.post('/github-webhook', async (req, res) => {
+  const githubEvent = req.headers['x-github-event'];
+  const payload = req.body;
+
+  if (githubEvent === 'push') {
+    const commitMessages = payload.commits.map(commit => commit.message).join('\n');
+    const repositoryName = payload.repository.full_name;
+    const pusherName = payload.pusher.name;
+
+    const message = `Repositório: ${repositoryName}\nPush realizado por: ${pusherName}\nCommits:\n${commitMessages}`;
+
+    await sendToAnyDiscord("https://weebhookbeta", message);
+    res.status(200).send('Notificação do GitHub processada.');
+  } else {
+    res.status(400).send('Evento não suportado.');
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
